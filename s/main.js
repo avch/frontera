85cfa706d07138d70a672fd0f67b9f243c6364fa
AVCH <webavch@gmail.com>
2015-02-16 21:42:28 +0500

    Changes for responsive and clear structure

--- s/main.js
+++ s/main.js
@@ -1,12 +1,24 @@
 'use strict';
 
-$.extend(true, $.fancybox.defaults, { 'helpers': { 'overlay': { 'locked': false } }, 'tpl': { 'closeBtn': '<a class="fancybox-item fancybox-close"></a>', 'next': '<a class="fancybox-nav fancybox-next"><span></span></a>', 'prev': '<a class="fancybox-nav fancybox-prev"><span></span></a>' } });
+$.extend(true,$.fancybox.defaults,{helpers:{overlay:{locked:false}},tpl:{closeBtn:'<a class="fancybox-item fancybox-close"></a>',next:'<a class="fancybox-nav fancybox-next"><span></span></a>',prev:'<a class="fancybox-nav fancybox-prev"><span></span></a>'}});
 
 $(document).ready(function(){
 
-	initUtilities();
+	initLayout();
 
+	initUtilities();
 });
+
+function initLayout() {
+	var $window = $(window),
+		$header = $('.header'),
+		$footer = $('.footer'),
+		$content = $('.content');
+
+	$window.on('resize', function() {
+		$content.css('min-height', $window.outerHeight() - $header.outerHeight() - $footer.outerHeight());
+	}).trigger('resize');
+}
 
 function initUtilities() {
 
@@ -14,8 +26,16 @@ function initUtilities() {
 
 	$('.slick').slick();
 
-	$('.format a[href^=#], a.scrollto[href^=#]').on('click', function() {
-		$('html, body').animate({ 'scrollTop': $(this.hash).offset().top }, 'slow');
-		return false;
-    });
+	$('.format a[href^=#], a.scrollto[href^=#]').each(function() {
+		var $target = $(this.hash);
+
+		$(this).on('click', function() {
+			if($target.length > 0) {
+				$('html, body').animate({ scrollTop: $target.offset().top }, 'slow');
+			}
+			return false;
+	    });
+	});
+
+	$('a[href=#no-click]').on('click', function() { return false; });
 }
